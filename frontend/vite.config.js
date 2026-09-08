95365fe529a2657de8fed30cb126da4458f359aa
ashoksirvi7795-stack <ashoksirvi7795@gmail.com>
2026-09-08 14:16:39 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/College_Management_System/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -13,4 +13,4 @@ export default defineConfig({
       }
     }
   }
-});
+})
